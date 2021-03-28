6f5bb77eb602696c1533401c07d4515478aa7c15
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-29 01:48:03 +0530

    Added: ability to edit contact details using name

--- addressBook/src/addressBook/AddressBook.java
+++ addressBook/src/addressBook/AddressBook.java
@@ -7,24 +7,18 @@ public class AddressBook
 {
 	
 		//creating variables
-	   String first_Name;
-	   String last_Name;
-	   String address;
-	   String city;
-	   String state;
-	   long zip_Code;
-	   long phone_Number;
-	   String email_Id;	
+	   String first_Name, last_Nam, address, city, state, email_Id;
+	   long zip_Code, phone_Number;
 	   
-	   
+	   		 Scanner input= new Scanner(System.in);
 	   		public void details()
 	   		{
-	   				 Scanner input= new Scanner(System.in);
-				   	 System.out.println("Enter your details:");
-				     System.out.println("Enter your First Name: ");
-					 this.first_Name=input.nextLine();
-					 System.out.println("Enter your Last Name: ");
-					 this.last_Name=input.nextLine();
+	   				
+				   	 System.out.println("Enter your details:\n");
+				     System.out.println("Enter your First Name:");
+				     this.first_Name=input.nextLine();
+				     System.out.println("Enter your Last Name: ");	 
+					 this.last_Nam=input.nextLine();
 					 System.out.println("Enter your Address: ");
 					 this.address=input.nextLine();
 					 System.out.println("City : ");
@@ -42,39 +36,162 @@ public class AddressBook
 	   	//return string for displaying the user input
 	 	public String toString()
 	 	{
-		 return (" first_Name: "+first_Name+"\n last_Name: "+last_Name+"\n address: "+address+"\n city: "+city+"\n state: "+state+"\n Zip Code: "+zip_Code+"\n phone_Number :"+phone_Number+"\n email_Id: "+email_Id+"\n");
+		 return (" first_Name: "+first_Name+"\n  last_Name: "+last_Nam+"\n address: "+address+"\n city: "+city+"\n state: "+state+"\n Zip Code: "+zip_Code+"\n phone_Number :"+phone_Number+"\n email_Id: "+email_Id+"\n");
+	 	}
+
+	 	public String getName()
+	 	{
+	 		return first_Name;
 	 	}
 	 	
-	
+	 	public void editDetails()
+   		{
+	 		
+	 		System.out.println("Which details you want to edit");
+	 		System.out.println("Press 1 for Name\n 2 Address\n 3 City\n 4 State\n 5 Zip Code\n 6 Phone Number\n 7 Email Id\n 8 Exit ");
+	 		Scanner edit_obj=new Scanner(System.in);
+	 		int edit=edit_obj.nextInt();
+	 		switch(edit) 
+	 		{
+			 		case 1: 
+			 			System.out.println("Press 1 to edit your Firstname\n press 2 to LastName\n press 0 for exit");
+			 			Scanner input1=new Scanner(System.in);
+			 			int option1=input1.nextInt();
+			 			switch(option1)
+			 			{				 					
+					 			case 1:
+					 					Scanner input2=new Scanner(System.in);
+					 					System.out.println("Enter the first Name");
+					 					String edit_firstName=input2.nextLine();
+					 					this.first_Name=edit_firstName;
+					 					break;
+					 			case 2:
+					 					Scanner input3=new Scanner(System.in);
+					 					System.out.println("Enter the Last Name");
+					 					String edit_lastName=input3.nextLine();
+					 					this.last_Nam=edit_lastName;
+					 					break;
+		 	
+					 			default:
+							 			break;
+				 	
+			 			}
+			 			break;
+			 			
+			 		case 2: 
+			 			Scanner input4=new Scanner(System.in);
+			 			System.out.println("Enter the Address");
+	 					String edit_Address=input4.nextLine();
+	 					this.address=edit_Address;
+	 					break;
+	 					
+			 		case 3: 
+			 			Scanner input5=new Scanner(System.in);
+			 			System.out.println("Enter the City");
+	 					String edit_City=input5.nextLine();
+	 					this.city=edit_City;
+	 					break;
+	 					
+			 		case 4: 
+			 			Scanner input6=new Scanner(System.in);
+			 			System.out.println("Enter the State");
+	 					String edit_State=input6.nextLine();
+	 					this.state=edit_State;
+	 					break;
+			 		case 5: 
+			 			Scanner input7=new Scanner(System.in);
+			 			System.out.println("Enter the Zip Code");
+	 					long edit_ZipCode=input7.nextLong();
+	 					this.zip_Code=edit_ZipCode;
+	 					break;
+			 		case 6: 
+			 			Scanner input8=new Scanner(System.in);
+			 			System.out.println("Enter the Phone number");
+	 					long edit_phoneNumber=input8.nextLong();
+	 					this.phone_Number=edit_phoneNumber;
+	 					break;
+			 		case 7: 
+			 			Scanner input9=new Scanner(System.in);
+			 			System.out.println("Enter the Email");
+	 					String edit_Email=input9.nextLine();
+	 					this.email_Id=edit_Email;
+	 					break;
+	 				default:
+	 					break;
+	 			}
+	 		}
 		 public static void main(String args[])
 			{
-				 ArrayList<AddressBook> User= new ArrayList<>();//creating Array list and User is object
-				 AddressBook person1=new AddressBook();//creating person1 object
-				 person1.details();//calling details method
-				 System.out.println(person1.toString());
-				 User.add(person1);//adding details into array list
-				 				 
-				 System.out.println("Do you want to add other person details Press 1 for Yes or Press 2 for exit");
+			 
+			 Scanner sc=new Scanner(System.in);
+			    System.out.println("Welcome to your address Book");
+				ArrayList<AddressBook> User= new ArrayList<>();//creating Array list and User is object
+				AddressBook person=new AddressBook();
+				AddressBook person1=new AddressBook();
+				
+				person.details();
+				person.toString();
+				User.add(person);
+				
 				 Scanner input1= new Scanner(System.in);
 				
+				 System.out.println("Please choose your choice");
+				 
+				 System.out.println("Press 1 for adding another details\n2 for exit");
+				
 				 int option=input1.nextInt();
+				 
 				 switch(option)
 				 {
-				 case 1:
-					AddressBook person2=new AddressBook();
-					person2.details();
-					//System.out.println(person2.toString());
-					User.add(person2);
-					break;
+					 case 1:
+						
+						 person1.details();
+						 person1.toString();
+						
+						   User.add(person1);
+						   break;
+						   
+				     default:
+							  break;
+				 }
 				 
-				 default:
-					 break;
-				 }
-				 //for loop for printing all the elements in the Array list(Contacts)
-				 for(int i=0;i<User.size();i++)
-				 {
-					System.out.println(User.get(i)); 
-				 }
-			}
+				 System.out.println("Press 1 for editing the details \n 2 for exit");
+					 Scanner press1=new Scanner(System.in);
+					 int options=press1.nextInt();
+					 switch(options)
+					 {
+					 case 1:
+						 	System.out.println("Enter the first name\n or else type exit ");
+						 	Scanner press=new Scanner(System.in);
+						 	String opt=press.nextLine();						 	
+						 	if(person.getName().equals(opt))
+						 		{								 		
+								 		person.editDetails();
+								 		person.toString();
+								 		User.set(0, person);
+						 		}
+						 	else if(person1.getName().equals(opt))
+						 	{
+						 	
+						 		person1.editDetails();
+						 		person1.toString();
+						 		User.set(1, person1);
+						 	}
+						 	else {
+						 		break;
+						 	}
+						 	default:
+						 		break;
+						 	}
+			
+				//for printing the list of contacts 
+				for(int i=0;i<User.size();i++)
+				{
+					
+					System.out.println(User.get(i));
+				}
+				
+			
+			}			
 }
 
