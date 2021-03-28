dcdb26aa6b6dde5df4de695ca17f6ec4f5d93eb4
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-29 02:28:09 +0530

    Added: Ability to delete the contact using the person name

--- addressBook/src/addressBook/AddressBook.java
+++ addressBook/src/addressBook/AddressBook.java
@@ -15,28 +15,28 @@ public class AddressBook
 	   		{
 	   				
 				   	 System.out.println("Enter your details:\n");
-				     System.out.println("Enter your First Name:");
-				     this.first_Name=input.nextLine();
-				     System.out.println("Enter your Last Name: ");	 
+				     System.out.println("Enter your First Name:");	
+					 this.first_Name=input.nextLine();
+		             System.out.println("Enter your Last Name: ");	 
 					 this.last_Nam=input.nextLine();
 					 System.out.println("Enter your Address: ");
 					 this.address=input.nextLine();
-					 System.out.println("City : ");
+					 System.out.println("Enter your City : ");
 					 this.city=input.nextLine();
-					 System.out.println("State : ");
+					 System.out.println("Enter your State : ");
 					 this.state=input.nextLine();
-					 System.out.println("Zip Code: ");
+					 System.out.println("Enter your Zip Code: ");
 					 this.zip_Code =input.nextLong();
-					 System.out.println("Phone Number: ");
+					 System.out.println("Enter your Phone Number: ");
 					 this.phone_Number =input.nextLong();
-					 System.out.println("Email Id : ");
+					 System.out.println("Enter your Email Id : ");
 					 this.email_Id=input.next();
 		 	}
 	 	
 	   	//return string for displaying the user input
 	 	public String toString()
 	 	{
-		 return (" first_Name: "+first_Name+"\n  last_Name: "+last_Nam+"\n address: "+address+"\n city: "+city+"\n state: "+state+"\n Zip Code: "+zip_Code+"\n phone_Number :"+phone_Number+"\n email_Id: "+email_Id+"\n");
+		 return (" First Name: "+first_Name+"\n Last Name: "+last_Nam+"\n Address: "+address+"\n City: "+city+"\n State: "+state+"\n Zip Code: "+zip_Code+"\n Phone Number :"+phone_Number+"\n Email_Id: "+email_Id+"\n");
 	 	}
 
 	 	public String getName()
@@ -98,45 +98,50 @@ public class AddressBook
 	 					String edit_State=input6.nextLine();
 	 					this.state=edit_State;
 	 					break;
+	 					
 			 		case 5: 
 			 			Scanner input7=new Scanner(System.in);
 			 			System.out.println("Enter the Zip Code");
 	 					long edit_ZipCode=input7.nextLong();
 	 					this.zip_Code=edit_ZipCode;
 	 					break;
+	 					
 			 		case 6: 
 			 			Scanner input8=new Scanner(System.in);
 			 			System.out.println("Enter the Phone number");
 	 					long edit_phoneNumber=input8.nextLong();
 	 					this.phone_Number=edit_phoneNumber;
 	 					break;
+	 					
 			 		case 7: 
 			 			Scanner input9=new Scanner(System.in);
 			 			System.out.println("Enter the Email");
 	 					String edit_Email=input9.nextLine();
 	 					this.email_Id=edit_Email;
 	 					break;
+	 					
 	 				default:
 	 					break;
 	 			}
 	 		}
-		 public static void main(String args[])
+	 	
+	 		public static void main(String args[])
 			{
 			 
-			 Scanner sc=new Scanner(System.in);
+	 			Scanner sc=new Scanner(System.in);
 			    System.out.println("Welcome to your address Book");
 				ArrayList<AddressBook> User= new ArrayList<>();//creating Array list and User is object
+				
 				AddressBook person=new AddressBook();
 				AddressBook person1=new AddressBook();
 				
-				person.details();
-				person.toString();
-				User.add(person);
+				person.details();//calling details method
+				person.toString();//calling toString method using obj
+				User.add(person);//adding to the ArrayList
 				
 				 Scanner input1= new Scanner(System.in);
 				
-				 System.out.println("Please choose your choice");
-				 
+				 System.out.println("Please choose your choice");				 
 				 System.out.println("Press 1 for adding another details\n2 for exit");
 				
 				 int option=input1.nextInt();
@@ -147,15 +152,14 @@ public class AddressBook
 						
 						 person1.details();
 						 person1.toString();
-						
-						   User.add(person1);
-						   break;
+					     User.add(person1);
+						 break;
 						   
 				     default:
-							  break;
+					     break;
 				 }
 				 
-				 System.out.println("Press 1 for editing the details \n 2 for exit");
+				     System.out.println("Press 1 for editing the details \n 2 for exit");
 					 Scanner press1=new Scanner(System.in);
 					 int options=press1.nextInt();
 					 switch(options)
@@ -163,34 +167,63 @@ public class AddressBook
 					 case 1:
 						 	System.out.println("Enter the first name\n or else type exit ");
 						 	Scanner press=new Scanner(System.in);
-						 	String opt=press.nextLine();						 	
-						 	if(person.getName().equals(opt))
-						 		{								 		
-								 		person.editDetails();
-								 		person.toString();
-								 		User.set(0, person);
-						 		}
-						 	else if(person1.getName().equals(opt))
-						 	{
+						 	String opt=press.nextLine();	
 						 	
-						 		person1.editDetails();
-						 		person1.toString();
-						 		User.set(1, person1);
-						 	}
-						 	else {
-						 		break;
-						 	}
-						 	default:
-						 		break;
-						 	}
+								 	if(person.getName().equals(opt))
+								 		{								 		
+										 		person.editDetails();
+										 		person.toString();
+										 		User.set(0, person);
+								 		}
+								 	else if(person1.getName().equals(opt))
+								 	{
+								 	
+								 		person1.editDetails();
+								 		person1.toString();
+								 		User.set(1, person1);
+								 	}
+								 	else {
+								 		break;
+								 		}
+								 	default:
+								 		break;
+					 }
+					 
+								 System.out.println("Press 1 for deleting a contact\n Press 2 for exit");
+								 Scanner delete=new Scanner(System.in);
+								 int deletion=delete.nextInt();
+								 switch(deletion)
+								 {
+								 case 1: 
+									 	System.out.println("Enter the first name of the contact for deletion\n or press exit for exit");
+									 	Scanner del=new Scanner(System.in);
+									 	String dele=del.nextLine();
+									 	
+											 	if(person.getName().equals(dele))
+											 	{
+											 		User.remove(person);
+											 	}
+											 	else if(person1.getName().equals(dele))
+											 	{
+											 		User.remove(person1);
+											 	}
+											 	else
+											 	{
+											 		break;
+											 	}
+											 	
+								 default:
+										break;
+								 }
 			
-				//for printing the list of contacts 
+				//for printing the list of contacts in Array List
+				System.out.println("Your Contacts in the Address Book:\n");
 				for(int i=0;i<User.size();i++)
 				{					
 					System.out.println(User.get(i));
 				}
 				
 			
-			}			
+		}			
 }
 
